30b7e18ca293915a622024019589a8aeb0b5575e
mfbutner <mfbutner@ucdavis.edu>
2023-10-27 18:41:51 -0700

    Fixed naming to work with my newer conventions

--- get_groups/__init__.py
+++ get_groups/__init__.py
@@ -3,40 +3,34 @@ import pathlib
 import pandas as pd
 
 path = str(pathlib.Path(__file__).parents[1])
-pathlib.Path(path+'/groups').mkdir(parents=True, exist_ok=True)
+pathlib.Path(path + '/groups').mkdir(parents=True, exist_ok=True)
 
 with open(f'{path}/get_groups/group_names.json') as f:
-    group_names=json.load(f)['group_names']
+    group_names = json.load(f)['group_names']
+
 
 class Groups:
-    def __init__(self,course):
+    def __init__(self, course):
         self.course = course
-        self.groups = [item for item in self.course.get_groups()]
+        group_categories = list(self.course.get_group_categories())
 
         print("")
         print("Generating Groups. This will take a little time.")
         for group_name in group_names:
             print(f"Group Name:{group_name}")
             fin_data = []
-            for group in self.groups:
-                if group_name in group.name:
-                    details = {}
-                    details['group_name'] = group.name
-                    users = [item for item in group.get_users()]
-                    details['groupmates'] = []
-                    details['email_ids'] = []
-                    for user in users:
-                        details['groupmates'] .append(user.name)
-                        details['email_ids'].append(user.integration_id)
-                    fin_data.append(details)
+            for group_category in group_categories:
+                if group_name in group_category.name:
+                    for group in group_category.get_groups():
+
+                        details = {}
+                        details['group_name'] = group.name
+                        users = [item for item in group.get_users()]
+                        details['groupmates'] = []
+                        details['email_ids'] = []
+                        for user in users:
+                            details['groupmates'].append(user.name)
+                            details['email_ids'].append(user.integration_id)
+                        fin_data.append(details)
             group_df = pd.DataFrame(fin_data)
-            group_df.to_csv(f'{path}/groups/{group_name}.csv',index=False)
-
-                    
-                    
-
-            
-
-        
-        
-
+            group_df.to_csv(f'{path}/groups/{group_name}.csv', index=False)
